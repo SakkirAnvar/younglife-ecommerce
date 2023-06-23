37dd739fc7a8fa316bcd28acff8d668c8fe34b5f
SakkirAnvar <sakkir.kokkadan33@gmail.com>
2023-06-23 22:26:01 +0530

    Final commit

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -4,8 +4,8 @@ import { useAuth } from '../context/auth'
 
 const HomePage = () => { 
     const [auth,setAuth] = useAuth()
-    return (
-            <Layout>
+  return (
+    <Layout>
                 <h1>HomePage</h1>
                 <pre>{JSON.stringify(auth, null, 4)}</pre>
             </Layout>
